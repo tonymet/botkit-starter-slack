9bdb24c1136a29967fc916f5aaee4f1af8c5be04
Anthony Metzidis <anthonym@earny.co>
2018-03-22 18:31:33 -0700

    updates gists

--- services/github.js
+++ services/github.js
@@ -1,16 +1,16 @@
 const debug = require('debug')('github-service')
 const GitHub  = require('github-api')
 // unauthenticated client 
-function createGist(content){
+function updateGist(filename,content){
     const gh = new GitHub({token: process.env.github_api_token});
-    let gist = gh.getGist(); // not a gist yet 
-    return gist.create({
+    let gist = gh.getGist(process.env.github_gist_id)
+    var params = {
         public: false,
         description: 'gist1',
-        files: {
-            "file1.txt": {
-                content: content
-            }
-        }})
+        files: {}
+    }
+    params.files[filename] = {content: content}
+    debug(params)
+    return gist.update(params)
 }
-module.exports = {createGist}
+module.exports = {updateGist}
--- skills/sample_events.js
+++ skills/sample_events.js
@@ -1,11 +1,14 @@
 var debug = require('debug')('sample_evens')
 var rp = require('request-promise-native')
+var {URL} = require('url')
 const githubService = require('../services/github.js')
 module.exports = function(controller) {
 
     controller.on('file_share', function(bot, file) {
             debug(file)
         if( file.file.url_private){
+            var private_url = new URL(file.file.url_private)
+            private_url.pathname_safe = private_url.pathname.replace(/\//g, '__')
             debug('url_private', file.file.url_private )
             var req = rp({
                 method: 'GET',
@@ -15,16 +18,13 @@ module.exports = function(controller) {
                 }
             }).then(res => {
                 debug("res", res)
-                return githubService.createGist(res) 
+                return githubService.updateGist(private_url.pathname_safe, res)
             }).then(githubResponse => {
                 debug(githubResponse)
-                bot.reply(file, 'created gist: ' + githubResponse.data.html_url)
+                bot.reply(file, 'updated gist: ' + githubResponse.data.html_url)
             }).catch(err => {
                 debug(err)
             })
         }
-        bot.reply(file , 'Welcome, <@' + file.user + '>');
-
     });
-
 }
